8c1d664cea17534dcd45f0a2fd7181c6582ef425
tiasha2007 <75356465+tiasha2007@users.noreply.github.com>
2023-03-06 22:35:05 +0530

    Add files via upload

---
+++ api.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, request
+from data import data
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+    return jsonify({
+        "data" : data,
+        "message" : "success"
+    }),200
+
+@app.route("/planet")
+def planet():
+    name = request.args.get("name")
+    planet_data = next(item for item in data if item["name"] == name)
+    return jsonify({
+        "data" : planet_data,
+        "message" : "success"
+    }),200
+
+if_name_ == "_main_":
+    app.run()
